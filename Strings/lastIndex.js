ebeffc8a071046f6ce513a07b314acf5ca415ade
Bidhu1024 <bidhu1024av@gmail.com>
2023-04-19 08:59:48 +0530

    lastIndex

---
+++ Strings/lastIndex.js
@@ -0,0 +1,13 @@
+const a = [1,2,3,4,5,6,12,3,4,4,5,6,6,1,2,3,4,5,6,7,2,3,4,4,5,6];
+const b = a.lastIndexOf(3);
+console.log(b);
+
+
+// Works in both arrays and strings, returns the last index occurence of the element passed
+
+
+let c = "Hello World"
+let d = c.lastIndexOf("l");
+console.log(d);
+
+//it takes only one argument
